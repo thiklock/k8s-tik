25e2ce3c0ecdd9670729b01d20ed177ec64aa760
thiklock <thiagoiklock@gmail.com>
2023-12-24 11:51:03 -0300

    fixes eror in index.mjs

--- k8s-web-hello/index.mjs
+++ k8s-web-hello/index.mjs
@@ -1,15 +1,15 @@
-import express from 'expres'
+import express from 'express' // Corrected import
 import os from 'os'
 
 const app = express()
 const PORT = 3000
 
 app.get("/", (req, res) => {
-	const helloMessage = `Hello from Big Chic and from the ${os.hostname()}`
-	console.log(helloMessage)
-	res.send(helloMessage)
+    const helloMessage = `Hello from Big Chic and from the ${os.hostname()}`
+    console.log(helloMessage)
+    res.send(helloMessage)
 })
 
-app.listen(PORT () => {
-	console.log(`Web server is listening at port ${PORT}`)
+app.listen(PORT, () => { // Corrected app.listen()
+    console.log(`Web server is listening at port ${PORT}`)
 })
